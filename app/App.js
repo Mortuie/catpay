2ea819df18701c6b0bfa5713af448ebb6250fcf8
mortuie <jimmybob-leon@hotmail.co.uk>
2018-10-27 02:53:05 +0200

    Working on the token retrieving

--- app/App.js
+++ app/App.js
@@ -1,5 +1,30 @@
-// in your index.ios.js or index.android.js
-import React, { Component } from "react";
-import App from "react-native-nfc-manager/example/App";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default App;
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import NFC from "./NFC";
+import styled from "styled-components/native";
+
+const MainContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5fcff;
+`;
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <NFC />
+      </View>
+    );
+  }
+}
--- app/NFC.js
+++ app/NFC.js
@@ -1,16 +1,143 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import NfcManager, { Ndef } from "react-native-nfc-manager";
 
 type Props = {};
 export default class Account extends Component<Props> {
   state = {
-    supported: false
+    supported: false,
+    parsedText: null
   };
 
-  componentDidMount;
+  componentDidMount() {
+    NfcManager.isSupported().then(supported => {
+      this.setState({ supported });
+      console.log("It's supported!");
+      if (supported) {
+        this._startNfc();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    if (this._stateChangedSubscription) {
+      this._stateChangedSubscription.remove();
+    }
+  }
 
   render() {
-    return <div />;
+    this._startDetection();
+    // <Button onPress={this._startDetection} title={"Press meeee"} />
+    return <Text>Token</Text>;
   }
+
+  _startNfc() {
+    NfcManager.start({
+      onSessionClosedIOS: () => {
+        console.log("ios session closed");
+      }
+    })
+      .then(result => {
+        console.log("start OK", result);
+      })
+      .catch(error => {
+        console.warn("start fail", error);
+        this.setState({ supported: false });
+      });
+
+    if (Platform.OS === "android") {
+      NfcManager.getLaunchTagEvent()
+        .then(tag => {
+          console.log("launch tag", tag);
+          if (tag) {
+            this.setState({ tag });
+          }
+          1997;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      NfcManager.isEnabled()
+        .then(enabled => {
+          this.setState({ enabled });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      NfcManager.onStateChanged(event => {
+        if (event.state === "on") {
+          this.setState({ enabled: true });
+        } else if (event.state === "off") {
+          this.setState({ enabled: false });
+        } else if (event.state === "turning_on") {
+          // do whatever you want
+        } else if (event.state === "turning_off") {
+          // do whatever you want
+        }
+      })
+        .then(sub => {
+          this._stateChangedSubscription = sub;
+        })
+        .catch(err => {
+          console.warn(err);
+        });
+    }
+  }
+
+  _onTagDiscovered = tag => {
+    console.log("Tag Discovered", tag);
+    this.setState({ tag });
+    let url = this._parseUri(tag);
+    if (url) {
+      Linking.openURL(url).catch(err => {
+        console.warn(err);
+      });
+    }
+
+    let text = this._parseText(tag);
+    console.log("TEXT: ", parseInt(text));
+    this.setState({ parsedText: text });
+  };
+
+  _startDetection = () => {
+    NfcManager.registerTagEvent(this._onTagDiscovered)
+      .then(result => {
+        console.log("registerTagEvent OK", result);
+      })
+      .catch(error => {
+        console.warn("registerTagEvent fail", error);
+      });
+  };
+
+  _stopDetection = () => {
+    NfcManager.unregisterTagEvent()
+      .then(result => {
+        console.log("unregisterTagEvent OK", result);
+      })
+      .catch(error => {
+        console.warn("unregisterTagEvent fail", error);
+      });
+  };
+
+  _parseText = tag => {
+    try {
+      if (Ndef.isType(tag.ndefMessage[0], Ndef.TNF_WELL_KNOWN, Ndef.RTD_TEXT)) {
+        return Ndef.text.decodePayload(tag.ndefMessage[0].payload);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    return null;
+  };
+
+  _parseUri = tag => {
+    try {
+      if (Ndef.isType(tag.ndefMessage[0], Ndef.TNF_WELL_KNOWN, Ndef.RTD_URI)) {
+        return Ndef.uri.decodePayload(tag.ndefMessage[0].payload);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    return null;
+  };
 }
